537a003a354b41a4897ee2d123d9da2ae7c259ba
Marcos García Quirós <phantomarko@gmail.com>
2019-12-30 09:00:44 +0100

    Add PHPUnit and Prophecy to project and create first test.

---
+++ tests/unit/Application/Product/Service/ProductToArrayConverterTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Tests\unit\Application\Product\Service;
+
+use App\Application\Product\Service\ProductToArrayConverter;
+use App\Domain\Product\Model\Product;
+use PHPUnit\Framework\TestCase;
+
+class ProductToArrayConverterTest extends TestCase
+{
+    private $productToArrayConverter;
+
+    public function setUp()
+    {
+        $this->productToArrayConverter = new ProductToArrayConverter();
+    }
+
+    public function testToArray()
+    {
+        $product = $this->prophesize(Product::class);
+        $uuid = 'uuid';
+        $product->uuid()->willReturn($uuid);
+        $name = 'name';
+        $product->name()->willReturn($name);
+        $description = 'description';
+        $product->description()->willReturn($description);
+        $price = 2.88;
+        $product->price()->willReturn($price);
+        $priceWithVat = 288.8;
+        $product->priceWithVat()->willReturn($priceWithVat);
+
+        $array = $this->productToArrayConverter->toArray($product->reveal());
+        
+        $this->assertIsArray($array);
+        $this->assertArrayHasKey('uuid', $array);
+        $this->assertSame($array['uuid'], $uuid);
+        $this->assertArrayHasKey('name', $array);
+        $this->assertSame($array['name'], $name);
+        $this->assertArrayHasKey('description', $array);
+        $this->assertSame($array['description'], $description);
+        $this->assertArrayHasKey('price', $array);
+        $this->assertSame($array['price'], $price);
+        $this->assertArrayHasKey('priceWithVat', $array);
+        $this->assertSame($array['priceWithVat'], $priceWithVat);
+    }
+}
